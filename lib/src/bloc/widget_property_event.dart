1891473ea22c3bbf0dece4a50457ea3f4aa5e613
rohan <rohan.kolkata@gmail.com>
2024-07-04 19:06:50 +0530

    lib code updated

--- lib/src/bloc/widget_property_event.dart
+++ lib/src/bloc/widget_property_event.dart
@@ -3,6 +3,7 @@ part of 'widget_property_bloc.dart';
 @immutable
 sealed class WidgetPropertyEvent {}
 
+// ignore: must_be_immutable
 class GetWidget extends WidgetPropertyEvent{
   Widget getWidget;
   GetWidget({required this.getWidget});
